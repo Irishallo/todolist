c53c8f7eb788afe022602dc0b818ae20de2a2acf
Iris <iclinde@xs4all.nl>
2021-07-29 15:11:24 +0200

    finished basic todo form

--- src/domnewtodo.js
+++ src/domnewtodo.js
@@ -1,6 +1,7 @@
 const domToDo = () => {
     const contentDiv = document.getElementById('content');
     const toDoForm = document.createElement('form');
+    toDoForm.setAttribute('id', 'createtodo');
     const toDoTable = document.createElement('table');
     const titleRow = document.createElement('tr');
     const titleTd1 = document.createElement('td');
@@ -20,6 +21,31 @@ const domToDo = () => {
     const descriptionInput = document.createElement('textarea');
     descriptionInput.setAttribute('id', 'descriptionin');
     descriptionInput.setAttribute('name', 'descriptionin');
+    const dateRow = document.createElement('tr');
+    const dateTd1 = document.createElement('td');
+    const dateTd2 = document.createElement('td');
+    const dateLabel = document.createElement('label');
+    dateLabel.setAttribute('for', 'datein');
+    dateLabel.innerText = 'Due Date';
+    const dateInput = document.createElement('input');
+    dateInput.setAttribute('id', 'datein');
+    dateInput.setAttribute('type', 'date');
+    const priorityRow = document.createElement('tr');
+    const priorityTd1 = document.createElement('td');
+    const priorityTd2 = document.createElement('td');
+    const priorityLabel = document.createElement('label');
+    priorityLabel.setAttribute('for', 'priorityin');
+    priorityLabel.innerText = 'Priority';
+    const priorityInput = document.createElement('input');
+    priorityInput.setAttribute('id', 'priorityin');
+    priorityInput.setAttribute('type', 'number');
+    priorityInput.setAttribute('min', '1');
+    priorityInput.setAttribute('max', '5');
+    priorityInput.setAttribute('value', '1');
+    const submitInput = document.createElement('input');
+    submitInput.setAttribute('id', 'priorityin');
+    submitInput.setAttribute('type', 'submit');
+    submitInput.setAttribute('value', 'submit');
     
     titleTd1.appendChild(titleLabel);
     titleTd2.appendChild(titleInput);
@@ -29,10 +55,21 @@ const domToDo = () => {
     descriptionTd2.appendChild(descriptionInput);
     descriptionRow.appendChild(descriptionTd1);
     descriptionRow.appendChild(descriptionTd2);
+    dateTd1.appendChild(dateLabel);
+    dateTd2.appendChild(dateInput);
+    dateRow.appendChild(dateTd1);
+    dateRow.appendChild(dateTd2);
+    priorityTd1.appendChild(priorityLabel);
+    priorityTd2.appendChild(priorityInput);
+    priorityRow.appendChild(priorityTd1);
+    priorityRow.appendChild(priorityTd2);
 
     toDoTable.appendChild(titleRow);
     toDoTable.appendChild(descriptionRow);
+    toDoTable.appendChild(dateRow);
+    toDoTable.appendChild(priorityRow);
     toDoForm.appendChild(toDoTable);
+    toDoForm.appendChild(submitInput);
     contentDiv.appendChild(toDoForm);
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import './style.css';
 import domToDo from './domnewtodo';
 
 const contentDiv = document.getElementById('content');
-
 const navBar = document.createElement('nav');
-
 const toDoDiv = document.createElement('div');
+const addToDoBtn = document.createElement('button');
+addToDoBtn.innerText = '+';
+addToDoBtn.setAttribute('id', 'addtodobtn');
+
+navBar.appendChild(addToDoBtn);
 
 contentDiv.appendChild(navBar);
-
 contentDiv.appendChild(toDoDiv);
 
-domToDo();
+addToDoBtn.addEventListener('click', () => {
+    const newToDoForm = document.getElementById('createtodo');
+    if(!contentDiv.contains(newToDoForm)) {
+        domToDo();
+        console.log('form is here');
+    } else {
+        contentDiv.removeChild(newToDoForm);
+        console.log('no form here');
+    }
+});
+
